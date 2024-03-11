03578ff9ee304f0410feee18e548b98c50868a92
Mohammad Raza Azad <mohammadrazaamu@gmail.com>
2024-03-11 17:50:09 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col item-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I am frontend developer
+            I am Full Stack developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I designed and developed a responsive portfolio website to showcase
